963ee013d314c7d2cfa164bbf3d5ef160d528fd1
behrad <bhradkhrazi03@gmail.com>
2025-08-31 03:40:51 +0330

    fixing shop tab

--- final_project/commodity/views.py
+++ final_project/commodity/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import product
 # Create your views here.
 def goods(request):
-    return render(request, 'shop.html')
+    products = product.objects.all()
+    return render(request, 'shop.html', {'products': products})
--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from shoping_cart.views import cart_view
@@ -30,4 +32,4 @@ urlpatterns = [
     path('commodity/', goods, name='goods'),
     path('id/', register, name='identification'),
     path('signin/', login_user, name='login'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
