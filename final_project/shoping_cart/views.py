8f73b417a8891860c539cd16d7cdd63412347b91
behrad <bhradkhrazi03@gmail.com>
2025-09-02 01:14:01 +0330

    sending id from shop to cart

--- final_project/shoping_cart/views.py
+++ final_project/shoping_cart/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.apps import apps
+from django.shortcuts import get_object_or_404
 # Create your views here.
-def cart_view(request):
-    return render(request, 'shop-single.html')
+def cart_view(request, id):
+    products = apps.get_model('commodity', 'product').objects.all()
+    requested_product = apps.get_model('commodity', 'product').objects.all()
+    requested_product = get_object_or_404(requested_product, id=id)
+    return render(request, 'shop-single.html', {'products': products, 'requested_product': requested_product})
