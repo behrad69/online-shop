242da139e5574ce1a2ea0dda648105efbbd75e6a
behrad <bhradkhrazi03@gmail.com>
2025-09-05 03:08:03 +0330

    a little clearing up

--- final_project/shoping_cart/views.py
+++ final_project/shoping_cart/views.py
@@ -9,19 +9,8 @@ def cart_view(request, id):
     requested_product = apps.get_model('commodity', 'product').objects.all()
     requested_product = get_object_or_404(requested_product, id=id)
     if request.user.is_authenticated:
-        # User is logged in
-        if request.method == 'POST':
-        # Collect all the data they entered
-            form = cartform(request.POST)
-        
-        # Check if the data is valid (correct email format, etc.)
-            if form.is_valid():
-            # Save the data to database
-                form.save()
-            # Redirect to a success page
         return render(request, 'shop-single.html', {'products': products, 'requested_product': requested_product})
     else:
-        # User is not logged in
         return redirect('login')
     
 def select_product(request, id):
@@ -37,8 +26,8 @@ def select_product(request, id):
             cart_item.product_name = requested_product.name  # From database  
             cart_item.user = request.user  # From logged in user
             form.save()
-            return HttpResponse('fuck')
+            return redirect('goods')
     else:
         form = cartform()
-        return HttpResponse('shit')
-    return HttpResponse('wtf')
+        return render(request, 'shop-single.html', {'products': products, 'requested_product': requested_product})
+    return render(request, 'shop-single.html', {'products': products, 'requested_product': requested_product})
